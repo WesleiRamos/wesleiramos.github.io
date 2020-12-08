1edf086f70c002c00821542f89d9c064247d1291
Weslei Ramos <wesleiramos2@gmail.com>
2020-12-08 00:21:34 -0300

    Adiciona mais musicas recomendadas

--- atividadegrid/js/dados.js
+++ atividadegrid/js/dados.js
@@ -121,4 +121,150 @@ const recommended = shuffleArray([
     artist: 'Milton Nascimento',
     artwork: './images/artwork/fazenda.jpg',
   },
+
+  {
+    name: 'Lemonade',
+    artist: 'Internet Money, Don Toliver, Gunna, NAV',
+    artwork: './images/artwork/lemonade.png',
+  },
+
+  {
+    name: 'Goosebumps',
+    artist: 'Travis Scott',
+    artwork: './images/artwork/goosebumps.jpg',
+  },
+
+  {
+    name: 'Sicko Mode',
+    artist: 'Travis Scott',
+    artwork: './images/artwork/sicko-mode.jpg',
+  },
+
+  {
+    name: 'No ideia',
+    artist: 'Don Toliver',
+    artwork: './images/artwork/no-ideia.jpg',
+  },
+
+  {
+    name: 'Through the fires and flames',
+    artist: 'DragonForce',
+    artwork: './images/artwork/dragon-force.jpg',
+  },
+  
+  {
+    name: 'Praise the lord',
+    artist: 'A$AP Rocky, Skepta',
+    artwork: './images/artwork/praise-the-lord.jpg',
+  },
+  
+  {
+    name: 'Big Love',
+    artist: 'EarthGang, Louis The Child',
+    artwork: './images/artwork/big-love.jpg',
+  },
+
+  {
+    name: 'So Good',
+    artist: 'Santti, Kiko Franco, Make U Sweat, Vic Brow',
+    artwork: './images/artwork/so-good.jpg',
+  },
+
+  {
+    name: 'Blinding Lights - Major Lazer Remix',
+    artist: 'The Weeknd, Major Lazer',
+    artwork: './images/artwork/blinding-lights.jpg',
+  },
+  
+  {
+    name: 'All Nights - Festival Mix',
+    artist: 'Afrojack, Ally Brooke',
+    artwork: './images/artwork/all-nights.jpg',
+  },
+
+  {
+    name: 'Learn To Lose',
+    artist: 'Bakermat',
+    artwork: './images/artwork/learn-to-lose.jpg',
+  },
+
+  {
+    name: 'Learn To Live',
+    artist: 'Alice Merton',
+    artwork: './images/artwork/mint.jpg',
+  },
+
+  {
+    name: 'No Roots',
+    artist: 'Alice Merton',
+    artwork: './images/artwork/mint.jpg',
+  },
+
+  {
+    name: 'Nuthin\' But a \'G\' Thang',
+    artist: 'Dr. Dre, Snoop Dogg',
+    artwork: './images/artwork/nuthin-g-thang.jpg',
+  },
+
+  {
+    name: 'To Voando Alto',
+    artist: 'Mc Poze Do Rodo, DJ Gabriel do Borel',
+    artwork: './images/artwork/to-voando-alto.jpg',
+  },
+
+  {
+    name: 'Vale Nada Vale Tudo',
+    artist: 'Ednaldo Pereira',
+    artwork: './images/artwork/vale-nada-vale-tudo.jpg',
+  },
+
+  {
+    name: 'Hoje Eu To Terrível',
+    artist: 'Cristiano Araújo',
+    artwork: './images/artwork/hj-eu-to-terrivel.jpg',
+  },
+
+  {
+    name: 'Sentimentos De Vaqueiro',
+    artist: 'Raí Saia Rodada',
+    artwork: './images/artwork/sentimentos-de-vaqueiro.jpg',
+  },
+
+  {
+    name: 'Telephone Number',
+    artist: 'Junko Ohashi',
+    artwork: './images/artwork/telephone-number.jpg',
+  },
+
+  {
+    name: 'Namorando e Me Querendo',
+    artist: 'Henrique & Juliano',
+    artwork: './images/artwork/namorando-e-me-querendo.jpg',
+  },
+
+  {
+    name: 'Nada Nada',
+    artist: 'Henrique & Juliano',
+    artwork: './images/artwork/namorando-e-me-querendo.jpg',
+  },
+
+  {
+    name: 'Tcc (Truco Cerveja e Churrasco)',
+    artist: 'Antony & Gabriel',
+    artwork: './images/artwork/tcc.jpg',
+  },
+
+  {
+    name: 'Tó Sou Seu',
+    artist: 'Fred & Gustavo, Wesley Safadão',
+    artwork: './images/artwork/to-sou-seu.jpg',
+  },
+
+  {
+    name: 'Playing To Lose',
+    artist: 'Lemaitre',
+    artwork: './images/artwork/playing-to-lose.jpg',
+  }
+
+  //
 ])
